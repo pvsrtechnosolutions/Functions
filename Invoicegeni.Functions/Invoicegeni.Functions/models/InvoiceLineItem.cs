ceb25a9cea224ad2df4a7e4abc8e3a2d1d487c2a
bharat1428 <159933688+bharat1428@users.noreply.github.com>
2025-07-30 09:57:08 +0530

    Update InvoiceLineItem.cs

--- Invoicegeni.Functions/Invoicegeni.Functions/models/InvoiceLineItem.cs
+++ Invoicegeni.Functions/Invoicegeni.Functions/models/InvoiceLineItem.cs
@@ -9,10 +9,12 @@ namespace Invoicegeni.Functions.models
     public class InvoiceLineItem
     {
         public string ItemDescription { get; set; }
+        public string ItemId { get; set; }
         public decimal Quantity { get; set; }
         public decimal UnitPrice { get; set; }
+        public decimal Amount { get; set; }
         public string UnitPriceCurrency { get; set; }
-
+    
     }
 
 }
